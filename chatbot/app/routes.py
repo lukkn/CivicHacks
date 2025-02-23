40f24661f24a826d73dbc2fc290cbb58d132b4a6
Tina <y634huan@uwaterloo.ca>
2025-02-22 22:15:37 -0500

    Deleted the simple flask REST API demo

--- chatbot/app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-main = Blueprint('main', __name__)
-
-
-@main.route('/api/endpoint', methods=['GET'])
-def example_endpoint():
-    return jsonify({"message": "Hello, world!"})
